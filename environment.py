2638d9c17b6b61390ba6f55dcc2df1e7674768c5
Joshua Santy <98499088+MrJoshie333@users.noreply.github.com>
2025-06-23 12:57:58 -0500

    Update environment.py

--- environment.py
+++ environment.py
@@ -9,8 +9,12 @@ class Time:
 
 class Environment:
     def __init__(self, gravity = 9.81, rho = 1.225, p = 101325, T = 288.15, a = 343):
+        #radius of the Earth, in m
+        self.earthRadius = 6371008.77
+        #standard gravitational acceleration constant, m/s^2
+        self.gravitationalConst = 9.80665
         #gravitational constant in m/s^2 (assuming unchanging with altitude)
-        self.gravity = gravity
+        self.gravitationalAcceleration = gravity
         #density of air in kg/m^3 (assuming unchanging with altitude)
         self.rho = rho
         #pressure of air in Pa (assuming unchanging with altitude)
